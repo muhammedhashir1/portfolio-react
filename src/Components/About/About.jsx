516647f58888dde492c6d6f7946e44eeaa8e069f
Muhammed Hashir <hashiraachi818@gmail.com>
2022-06-30 08:31:26 +0530

    about session processing

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
-import './About.css'
+import React from "react";
+import "./About.css";
+import ME from "../../Assets/about.jpg";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { GoProject } from "react-icons/go";
 const About = () => {
   return (
-    <section id='about'>About</section>
-  )
-}
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
 
-export default About
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="about me" />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>Fresher</small>
+            </article>
+            <article className="about__card">
+              <FiUsers className="about__icon" />
+              <h5>Clients</h5>
+              <small>0</small>
+            </article>
+            <article className="about__card">
+              <GoProject className="about__icon" />
+              <h5>P44rojects</h5>
+              <small>6</small>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;

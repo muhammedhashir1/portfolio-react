a8c2f46bf3ca3e90ae330de814fab796ec31b69f
Muhammed Hashir <hashiraachi818@gmail.com>
2022-07-18 17:08:43 +0530

    footer section started

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,7 +3,23 @@ import './Footer.css'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__logo'>Muhammed Hashir</a>
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+      <div className="footer__socials">
+        <a href="https://facebook.com"></a>
+        <a href="https://instagram.com"></a>
+        <a href="https://twitter.com"></a>
+      </div>
+    </footer>
   )
 }
 

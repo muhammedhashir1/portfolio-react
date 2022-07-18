9d8a51799e59183fe7737fe7f8926ca33470e9f5
Muhammed Hashir <hashiraachi818@gmail.com>
2022-07-18 16:02:18 +0530

    contact  section almost completed(but the changes saved in testimonial commit,my mistake)

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,7 +1,43 @@
 import React from "react";
 import "./Contact.css";
+import {MdOutlineMail} from 'react-icons/md'
+import {BsInstagram} from 'react-icons/bs'
+import {BsWhatsapp} from 'react-icons/bs'
+
 const Contact = () => {
-  return <section id="contact">Contact</section>;
+  return <section id="contact">
+    <h5>Get in Touch</h5>
+    <h2>Contact Me</h2>
+    <div className="container contact__container">
+      <div className="contact__options">
+        <article className="contact__option">
+          <MdOutlineMail className="contact__option-icon"/>
+          <h4>Email</h4>
+          <h5>Hashiraachi818@gmail.com</h5>
+          <a href="mailto:Hashiraachi818@gmail.com" target='_blank'>Send a Message</a>
+        </article>
+        <article className="contact__option">
+          <BsInstagram className="contact__option-icon"/>
+          <h4>instagram</h4>
+          <h5>muhammed_hashir___</h5>
+          <a href="https://www.instagram.com/muhammed_hashir___/ target='_blank'">Send a Message</a>
+        </article>
+        <article className="contact__option">
+          <BsWhatsapp className="contact__option-icon"/>
+          <h4>WhatsApp</h4>
+          <h5>+918089462122</h5>
+          <a href="https://wa.me/918089462122" target='_blank'>Send a Message</a>
+        </article>
+      </div>
+      {/*END OF CONTACT OPTION*/}
+      <form action="">
+        <input type="text" name="name" placeholder="Your Full Name"  required/>
+        <input type="email" name="email" placeholder="Your Email"  required/>
+        <textarea name="Message" rows="7" placeholder="Your Message" required></textarea>
+        <button type="submit" className="btn btn-primary">Send Message</button>
+      </form>
+    </div>
+  </section>;
 };
 
 export default Contact;
